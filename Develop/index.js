61c8a554e847c60ccd419b43f600371fd4f5802c
JERSEY JACKPOT <JERSEYJACKPOT@GMAIL.COM>
2020-07-27 15:04:48 -0400

    installed inquirer, added example question

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,28 @@
-// array of questions for user
-const questions = [
+const inquirer = require(`inquirer`);
 
-];
+
+// array of questions for user
+inquirer.prompt= ([
+    {
+        type: `input`,
+        message: "What is your username?",
+        name: `user`,
+        validate: value => {
+            if (user.length < 8 ){
+                return "Username is too short.";           
+            }
+            else if(user.toLowerCase () != user){
+                return "Username should be lowercase.";
+            }
+            else{
+                //all validation checks passed
+                return true;
+            }
+        }
+    }
+]).then( response => {
+    console.log(response);
+});
 
 // function to write README file
 function writeToFile(fileName, data) {
