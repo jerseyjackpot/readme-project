26bb14a4dfeb74d5ac04789f874f5210b7285ea7
JERSEY JACKPOT <JERSEYJACKPOT@GMAIL.COM>
2020-07-28 00:20:56 -0400

    validation updates

--- Develop/index.js
+++ Develop/index.js
@@ -1,23 +1,27 @@
-const inquirer = require ('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
+const specialchar = "!#$%&()*+,./:;<=>?@^[\\]^_`{|}~".split("");
 
 // const api = require('./utils/api.js')
 // const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // array of questions for user
-inquirer.prompt ([
+inquirer.prompt([
     {
         type: 'input',
         name: 'user',
         message: "What is your GitHub username?",
-        
+
         validate: user => {
-            if (user.length < 8 ){
-                return "Username is too short.";           
+            if (user.length < 1 && user.length > 39) {
+                return "User name must be between 1 and 39 characters, please try again.";
             }
-            else if (user.toLowerCase () != user){
-                return "Username should be lowercase.";
+            else if (user.startsWith("-", 0)) {
+                return "Username cannot start with a dash";
             }
+            // else if (user.length.includes(specialchar)) {
+            //     return "Username cannot contain special characters, only alpha/numeric and dashes...please try again";
+            // }
             else {
                 //all validation checks passed
                 return true;
@@ -29,36 +33,60 @@ inquirer.prompt ([
         message: 'Please enter your password',
         name: 'secretcode',
         validate: secretcode => {
-            if (secretcode.length < 8){
+            if (secretcode.length < 8) {
                 return "Password is too short."
             }
-            else if(secretcode.length > 32){
+            else if (secretcode.length > 32) {
                 return "Password is too long."
             }
             else {
                 return true;
             }
-            
-            
+
+
         }
-        
-    }
+
+    } 
+    // {
+    //     type: 'input',
+    //     message: 'Please enter your project title',
+    //     name: 'projTitle',
+    //     validate: projTitle => {
+    //         if (projTitle.length < ) {
+    //             return "Password is too short."
+    //         }
+    //         else if (secretcode.length > 32) {
+    //             return "Password is too long."
+    //         }
+    //         else {
+    //             return true;
+    //         }
+
+
+    //     }
+
+    // }
     // {
     //     type: 'input',
     //     name: 'gitrepo',
     //     message: 'What is your repository name?'
-    
-    
-    // },
-    
+    //     validate: await api.getUser(user, gitrepo)
+    //         if(!user) {
+    //         console.error('Repo not found!')
+    //         } else {
+    //         console.log(`${ Object.fullName} found!`)
+    //         }
 
-]).then( response => {
+    // }
+
+
+]).then(response => {
     console.log(response.user);
     console.log(response.august);
 
-    fs.writeFile("readme.md", JSON.stringify(response, null, 2), 'utf8', err =>{
-        if(err) return console.log(err);
-        return console.log ("We finished writing the file.");
+    fs.writeFile("readme.md", JSON.stringify(response, null, 2), 'utf8', err => {
+        if (err) return console.log(err);
+        return console.log("We finished writing the file.");
     })
 });
 
@@ -73,3 +101,7 @@ inquirer.prompt ([
 
 // // function call to initialize program
 // init();
+
+
+// with the title of your project and 
+// sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
