3aca06fc70180efa52a07796697706ec6a6b14a4
JERSEY JACKPOT <JERSEYJACKPOT@GMAIL.COM>
2020-07-27 22:04:51 -0400

    added password prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,37 +1,75 @@
-const inquirer = require(`inquirer`);
+const inquirer = require ('inquirer');
+const fs = require('fs');
 
+// const api = require('./utils/api.js')
+// const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // array of questions for user
-inquirer.prompt= ([
+inquirer.prompt ([
     {
-        type: `input`,
-        message: "What is your username?",
-        name: `user`,
-        validate: value => {
+        type: 'input',
+        name: 'user',
+        message: "What is your GitHub username?",
+        
+        validate: user => {
             if (user.length < 8 ){
                 return "Username is too short.";           
             }
-            else if(user.toLowerCase () != user){
+            else if (user.toLowerCase () != user){
                 return "Username should be lowercase.";
             }
-            else{
+            else {
                 //all validation checks passed
                 return true;
             }
         }
+    },
+    {
+        type: 'password',
+        message: 'Please enter your password',
+        name: 'secretcode',
+        validate: secretcode => {
+            if (secretcode.length < 8){
+                return "Password is too short."
+            }
+            else if(secretcode.length > 32){
+                return "Password is too long."
+            }
+            else {
+                return true;
+            }
+            
+            
+        }
+        
     }
+    // {
+    //     type: 'input',
+    //     name: 'gitrepo',
+    //     message: 'What is your repository name?'
+    
+    
+    // },
+    
+
 ]).then( response => {
-    console.log(response);
+    console.log(response.user);
+    console.log(response.august);
+
+    fs.writeFile("readme.md", JSON.stringify(response, null, 2), 'utf8', err =>{
+        if(err) return console.log(err);
+        return console.log ("We finished writing the file.");
+    })
 });
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
 
-// function to initialize program
-function init() {
+// // function to initialize program
+// function init() {
 
-}
+// }
 
-// function call to initialize program
-init();
+// // function call to initialize program
+// init();
---
+++ Develop/readme.md
@@ -0,0 +1,4 @@
+{
+  "user": "jerseyjackpot",
+  "secretcode": "front242"
+}
