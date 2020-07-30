cb7647dcf52cdefec51e281cd662fafd15654d59
JERSEY JACKPOT <JERSEYJACKPOT@GMAIL.COM>
2020-07-30 02:33:30 -0400

    updated markdown to include Table of contents

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,52 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+const usage =(info)=>{
+  if(info !== 'None'){
+    return `This project is licensed under ${info} license.`;
+  }
+  return '';
+}
+
+// function to generate markdown for README
+const generateMarkdown = (profileData) => {
+  return `
+# ${profileData.projTitle}
+
+## INFO
+
+## Description 
+
+${profileData.description}
+
+## Table of Contents 
+
+* [Installation](#Installation)
+
+* [Usage](#Usage)
+
+* [Tests](#Tests)
+
+* [Questions](#Questions)
+
+
+### Installation ${profileData.instruct}
+
+### Usage ${profileData.usage}
+
+## License ${profileData.license}
+
+### Contributing ${profileData.guidelines}
+
+### Tests 
+To run tests, run the command:
+
+````
+${profileData.test}
+````
+### Questions 
+
+If you have any questions about the repo, open an issue or contact ${profileData.user} directly at [${profileData.email}]
+
+  `;
 }
 
 module.exports = generateMarkdown;
