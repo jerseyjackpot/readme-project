7797732071140d01f4b8f152328b738ee294980f
JERSEY JACKPOT <JERSEYJACKPOT@GMAIL.COM>
2020-07-30 02:32:53 -0400

    installed axios

--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,7 +1,7 @@
-const axios = require('axios')
+const axios = require('axios');
 const api = {
-  getUser: function(username) {
-    return axios.get(`https://api.github.com/users/${username}`)
+  getUser: function(user) {
+    return axios.get(`https://api.github.com/users/${user}`)
   }
 };
 
